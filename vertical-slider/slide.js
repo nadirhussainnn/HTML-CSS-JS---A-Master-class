5ef5d33bba3927e990cab0629d0bd4c2ffcb1f94
nadirhussainnn <nadirhussaintumrani@gmail.com>
2022-09-20 07:09:48 -0700

    created canvas

---
+++ vertical-slider/slide.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {  
+    // get one from 
+    // https://alvarotrigo.com/fullPage/pricing/
+    licenseKey: 'YOUR KEY HERE',
+  
+    navigation: true,
+    sectionsColor: ['#ff5f45', '#0798ec', '#fc6c7c', '#fec401'],
+  });
